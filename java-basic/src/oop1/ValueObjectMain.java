c123c6723ddf83c2c8597fc615b8204a78d70dd1
ralph <sanghwaj222@gmail.com>
2024-08-29 14:09:01 +0900

    0829 JAVA basic

---
+++ java-basic/src/oop1/ValueObjectMain.java
@@ -0,0 +1,11 @@
+package oop1;
+
+public class ValueObjectMain {
+    public static void main(String[] args) {
+        ValueData data = new ValueData();
+
+        data.add();
+
+        System.out.println(data.value);
+    }
+}
